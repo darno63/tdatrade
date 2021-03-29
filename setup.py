901b097cafdbb303aba30b1dce8dd7756426a732
Dave <Darno63@gmail.com>
2021-03-29 15:13:37 -0400

    add callback functions to webclient

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
       name='tdatrade',
       version='0.3',
       url='https://github.com/darno63/tdatrade.git',
-      packages=['tdatrade']
+      packages=find_packages(),
+      install_requires=['requests', 'websockets']
 )
--- tdatrade/streamdata/websocket.py
+++ tdatrade/streamdata/websocket.py
@@ -10,30 +10,35 @@ class WebSocketClient(Principals):
   def __init__(self):
     super().__init__()
     self.connection = None
+    self.callback_func = None
 
   async def connect_stream(self):
     # Collecting uri and requests
     login_req = self.login()
     uri = self.uri()
 
-    socket = await websockets.client.connect(uri)
-    await socket.send(login_req)
-    print('sending request')
-
-    res = await socket.recv()
-    print(json.loads(res))
-
-    await socket.send(self.subscriptions())
-    while True:
-        res = await socket.recv()
-        print(json.loads(res))
-
-  async def connect(self):
-    uri = self.uri()
     self.connection = await websockets.client.connect(uri)
     if self.connection.open:
         print("Connection established. Client correctly connected.")
-        return self.connection
+
+    await self.connection.send(login_req)
+    print('sending request')
+
+    res = await self.connection.recv()
+    print(json.loads(res))
+
+    await self.connection.send(self.subscriptions())
+    task = asyncio.create_task(self.receiveMessage())
+    await task
+    """
+    await self.receiveMessage()
+    while True:
+        res = await connection.recv()
+        print(json.loads(res))
+    """
+  
+  def connect(self):
+    asyncio.run(self.connect_stream())
 
   async def receiveMessage(self):
     while True:
@@ -41,15 +46,24 @@ class WebSocketClient(Principals):
           # grab and decode the message
           message = await self.connection.recv()
           message_decoded = json.loads(message)
+          if self.callback_func is not None:
+            self.callback_func(message_decoded)
+
           if 'notify' not in message_decoded:
             print(message_decoded)
         
         except websockets.exceptions.ConnectionClosed:
           print("Connection with server closed")
           break
+  
+  def create_callback(self, function):
+    self.callback_func = function
+
+  
         
 if __name__ == '__main__':
   client = WebSocketClient()
   client.forex_lvl1('EUR/USD')
   client.future_lvl1('/ES')
-  asyncio.run(client.connect_stream())
+  #client.chart_futures('/ES')
+  client.connect()
